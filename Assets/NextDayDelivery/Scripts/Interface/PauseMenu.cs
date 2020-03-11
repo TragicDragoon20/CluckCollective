631ef1e7e492308c572222bf30385e550381d56e
Benjamin Emdon <57227860+BenjaminEmdon@users.noreply.github.com>
2020-03-11 17:06:18 +0000

    pause menu continue now removes cursor

--- Assets/NextDayDelivery/Scripts/Interface/PauseMenu.cs
+++ Assets/NextDayDelivery/Scripts/Interface/PauseMenu.cs
@@ -28,6 +28,9 @@ public class PauseMenu : MonoBehaviour
 
     public void Resume()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
