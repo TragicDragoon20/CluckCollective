ab86166e6148aac03f305d2d8bc1ed5e27e312a4
James Heath <jh230155@falmouth.ac.uk>
2020-04-10 14:50:26 +0100

    Player can now climb ladders. I have added three new scripts for this. Climb.cs PlayerBottomCheck.cs PlayerTopCheck.cs

---
+++ Assets/NextDayDelivery/Scripts/Player/Climb.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Climb : MonoBehaviour
+{
+    public GameObject player;
+
+    public GameObject bottom;
+    public GameObject top;
+
+    public bool atBottom = false;
+    public bool atTop = false;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            if(atBottom)
+            {
+                player.transform.position = top.transform.position;
+                atBottom = false;
+            }
+
+            if(atTop)
+            {
+                player.transform.position = bottom.transform.position;
+                atTop = false;
+            }
+            
+            else
+            {
+
+            }
+        }
+    }
+}
---
+++ Assets/PlayerTopCheck.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTopCheck : MonoBehaviour
+{
+    public GameObject stepLadder;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            stepLadder.GetComponent<Climb>().atTop = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            stepLadder.GetComponent<Climb>().atTop = false;
+        }
+    }
+}
