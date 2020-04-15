23087763b8f20664b70ab1b229e3938fa3c4c4ee
Callum Metcalfe <cm227781@falmouth.ac.uk>
2020-04-15 17:23:53 +0100

    sound projector will now be destroyed after spawning

--- Assets/NextDayDelivery/Scripts/World Objects/EmitSound.cs
+++ Assets/NextDayDelivery/Scripts/World Objects/EmitSound.cs
@@ -19,8 +19,15 @@ public class EmitSound : MonoBehaviour
 
     [SerializeField]
     private GameObject audioProjection;
+    [SerializeField]
     private GameObject projector;
 
+    private Collider[] hits;
+
+    private bool timerRunning = false;
+    [SerializeField]
+    private float timeStart;
+    private float timeRemaining;
     private void Awake()
     {
         rb = this.GetComponent<Rigidbody>();
@@ -28,46 +35,80 @@ public class EmitSound : MonoBehaviour
 
 
     }
+
+    private void Update()
+    {
+        if (timerRunning)
+        {
+            if(timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+            }
+            else
+            {
+                timerRunning = false;
+                Destroy(projector);
+            }
+        }    
+    }
+
     private void FixedUpdate()
     {
         velocity = rb.velocity;
     }
     private void OnCollisionEnter(Collision collision)
     {
-        if (objPickUp.wasThrown)
+        if (objPickUp.wasThrown == true)
         {
-            if (velocity.z < 1 )
+            
+            if (velocity.y < 0.2f && velocity.x < 0.2f && velocity.z < 0.2f)
             {
-                origin = this.transform.position;
-                this.transform.rotation = new Quaternion(this.transform.rotation.x, 0, this.transform.rotation.z, 0);
-                
-
-
-                Collider[] hits = Physics.OverlapSphere(origin, sphereRadius, layerMask, QueryTriggerInteraction.UseGlobal);
-                int i = 0;
-                while (i < hits.Length)
-                {
-                    if (hits[i].gameObject.GetComponent<EnemyAI>() != null)
-                    {
-
-                        enemy = hits[i].gameObject.GetComponent<EnemyAI>();
-                        fOVDetection = hits[i].gameObject.GetComponent<FOVDetection>();
-                        if (enemy.state != EnemyAI.State.ChaseTarget)
-                        {
-                            fOVDetection.playerLastKnownPos = origin;
-                            fOVDetection.playerLastKnownPos.y += 1;
-                            enemy.state = EnemyAI.State.Sound;
-                            projector = Instantiate(audioProjection, new Vector3(this.transform.position.x, this.transform.position.y + 5, this.transform.position.z), Quaternion.Euler(90f, 0f, 0f));
-                            objPickUp.wasThrown = false;
-                        }
-                        
-                    }
-                    i++;
-                }
+                objPickUp.wasThrown = false;
+                GetEnemiesInRange();
             }
-            
-            
         }
         
+    }
+    private void SpawnProjectors()
+    {
+        this.transform.rotation = new Quaternion(this.transform.rotation.x, 0, this.transform.rotation.z, 0);
+        enemy.state = EnemyAI.State.Sound;
+        if(projector == null)
+        {
+            projector = Instantiate(audioProjection, new Vector3(this.transform.position.x, 3.05f, this.transform.position.z), Quaternion.Euler(90f, 0f, 0f));
+        }
+        else
+        {
+            Debug.Log("!");
+        }
+        timeRemaining = timeStart;
+        timerRunning = true;
+        
+
+    }
+    private void GetEnemiesInRange()
+    {
+        Collider[] hits = Physics.OverlapSphere(origin, sphereRadius, layerMask, QueryTriggerInteraction.UseGlobal);
+        
+        int i = 0;
+        while (i < hits.Length)
+        {
+            if (hits[i].gameObject.GetComponent<EnemyAI>() != null)
+            {
+
+                enemy = hits[i].gameObject.GetComponent<EnemyAI>();
+                fOVDetection = hits[i].gameObject.GetComponent<FOVDetection>();
+                if (enemy.state != EnemyAI.State.ChaseTarget)
+                {
+                    origin = this.transform.position;
+                    fOVDetection.playerLastKnownPos = origin;
+                    fOVDetection.playerLastKnownPos.y += 1;
+                    SpawnProjectors();
+                }
+
+            }
+            i++;
+
+        }
     }
 }
