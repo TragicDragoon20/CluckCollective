cbc4deec8251f0d6d959b71e5d35bc5e0ad408a6
James Heath <jh230155@falmouth.ac.uk>
2020-04-15 16:02:53 +0100

    Created FireDoor.cs It is a copy of DoorRegular.cs at the moment. Using it to create a new door script, which will allow doors to be only opened from one side. Later though.

---
+++ Assets/NextDayDelivery/Scripts/Doors/FireDoor.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireDoor : MonoBehaviour
+{
+    Animator anim;
+    //bool Opening;
+
+    public bool Locked;
+    public bool Opened = false;
+
+    public GameObject key;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    //public void OnTriggerEnter(Collider other)
+    //{
+    //if (other.gameObject == key)
+    //{
+    //    Debug.Log("Unlocked!!!!");
+    //    Locked = false;
+    //}
+    public void Interact()
+    {
+        if (!Opened)
+        {
+            if (Locked == false)
+            {
+                anim.SetBool("Open?", true);
+                Opened = true;
+            }
+            else
+            {
+                anim.SetTrigger("Locked");
+                Debug.Log("locked!!!");
+            }
+        }
+
+        else if (Opened)
+        {
+            anim.SetBool("Open?", false);
+            Opened = false;
+        }
+    }
+
+    //}
+
+    void OnTriggerExit()
+    {
+        //anim.SetBool("Open?", false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
