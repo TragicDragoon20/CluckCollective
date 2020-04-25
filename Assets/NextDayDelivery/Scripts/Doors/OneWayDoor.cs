3bcf84aebab219517d3593cb34368b871434ad43
James Heath <jh230155@falmouth.ac.uk>
2020-04-25 16:21:22 +0100

    OneWayDoor.cs to replace FireDoor.cs These doors will open automatically when the player enters their trigger.

---
+++ Assets/NextDayDelivery/Scripts/Doors/OneWayDoor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneWayDoor : MonoBehaviour
+{
+    Animator anim;
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        anim.SetBool("Open?", true);
+    }
+}
