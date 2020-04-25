956e88ef4cf5389f86e4a22335f33c7d3464e5e7
Callum Metcalfe <cm227781@falmouth.ac.uk>
2020-04-25 15:46:17 +0100

    Lights can now be dropped by changing a bool in the inspector

---
+++ Assets/NextDayDelivery/Scripts/World Objects/DroppingObjects.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroppingObjects : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField]
+    private bool canDrop;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canDrop)
+        {
+            rb.isKinematic = false;
+        }
+    }
+}
