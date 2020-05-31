3e03e8cf3fca6912b86bf1703de9ad1dddc85bb5
hamOverlord <30599923+hamOverlord@users.noreply.github.com>
2020-05-31 23:02:58 +0100

    Minigame looks decent now

--- Assets/NextDayDelivery/RhythmGame/Script/Keypress.cs
+++ Assets/NextDayDelivery/RhythmGame/Script/Keypress.cs
@@ -6,10 +6,18 @@ using TMPro;
 public class Keypress : MonoBehaviour
 {
     public GameObject space;
+    //public AudioSource audio;
+    public GameObject circle;
     public string keyType;
     private string[][] letterlayers = new string[][] { new[] { "z", "v", "m"},
         new[] { "a", "f", "j", "l", " "},
         new[] { "q", "r", "u", "p"} };
+
+    public Color32 col_w = new Color32(0x73, 0xEF, 0x5E, 0xFF);
+    public Color32 col_d = new Color32(0xED, 0x5F, 0x5F, 0xFF);
+    public Color32 col_p = new Color32(0xBF, 0x5F, 0xED, 0xFF);
+    public Color32 col_l = new Color32(0x60, 0xC3, 0xEA, 0xFF);
+    public Color32 col_y = new Color32(0xED, 0xE6, 0x5F, 0xFF);
 
     private void Start()
     {
@@ -28,11 +36,41 @@ public class Keypress : MonoBehaviour
     {
         transform.Translate(-Vector3.right * (Time.deltaTime)); //Moves each note left slowly
         TextMeshPro textmeshPro = GetComponent<TextMeshPro>(); //references the textmeshpro script attatched to the note
-        textmeshPro.SetText(keyType); //sets the text attatched to textmesh pro as the key type, which is assigned in the spawner script
+        textmeshPro.SetText(keyType);
+        Color keycolor = circle.GetComponent<SpriteRenderer>().color;
+        //sets the text attatched to textmesh pro as the key type, which is assigned in the spawner script
+        switch (keyType)
+        {
+            default:
+                circle.GetComponent<SpriteRenderer>().color = col_w;
+                Debug.Log("Couldn't find a colour so just used green");
+                break;
+            case "w":
+                circle.GetComponent<SpriteRenderer>().color = col_w;
+                Debug.Log("W uses green");
+                break;
+            case "d":
+                circle.GetComponent<SpriteRenderer>().color = col_d;
+                Debug.Log("D uses red");
+                break;
+            case "p":
+                circle.GetComponent<SpriteRenderer>().color = col_p;
+                Debug.Log("P uses purple");
+                break;
+            case "l":
+                circle.GetComponent<SpriteRenderer>().color = col_l;
+                Debug.Log("L uses blue");
+                break;
+            case "y":
+                circle.GetComponent<SpriteRenderer>().color = col_y;
+                Debug.Log("space uses yellow");
+                break;
+        }
 
         if (keyType == "space")
         {
             textmeshPro.fontSize = 0;
+            circle.SetActive(false);
             Instantiate(space, gameObject.transform);
 
         }
